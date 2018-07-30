f652f28f27fb2d4891dbfd83489a3dc3a6960494
AdrienM <amaurel90@gmail.com>
2018-07-30 12:22:21 +0000

    Update Install_Rancher.sh

--- Setup/Install_Rancher.sh
+++ Setup/Install_Rancher.sh
@@ -9,8 +9,9 @@ apt-get upgrade
 
 apt-get install -y fail2ban ufw nano deborphan apt-utils build-essential bridge-utils git sudo apt-transport-https ca-certificates curl software-properties-common
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+curl -fsSL get.docker.com -o get-docker.sh
+sh get-docker.sh
+
 apt-get update
 apt-get install docker-ce
 
