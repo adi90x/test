e049cd2d6f2a88bbc9c1b9a435e39e8db060538e
AdrienM <amaurel90@gmail.com>
2018-12-25 18:54:38 +0000

    Update Install_Rancher.sh

--- Setup/Install_Rancher.sh
+++ Setup/Install_Rancher.sh
@@ -53,6 +53,9 @@ sudo chown -R 102:105 /home/adrienm/data/rancher
 
 #Remove useless DNS service for iodined to work
 sudo sh -c 'echo "DNSStubListener=no" >> /etc/systemd/resolved.conf'
+sudo sh -c 'echo "nameserver 8.8.8.8" >> /etc/resolv.conf'
+sudo sh -c 'echo "nameserver 8.8.4.4" >> /etc/resolv.conf'
+
 sudo systemctl daemon-reload
 sudo systemctl restart systemd-resolved.service
 
